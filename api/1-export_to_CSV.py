e31721f6d5bb6619db2cfaea08ab04df667b58a7
Esteban Enriquez <juan.esteban.94@outlook.com>
2023-04-07 13:40:36 -0500

    task 1

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module"""
+
+import csv
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    todos_url = user_url + "/todos/"
+
+    user_info = requests.request('GET', user_url).json()
+    todos_info = requests.request('GET', todos_url).json()
+    emp_name = user_info["name"]
+    task_completed = list(filter(lambda obj:
+                                 (obj["completed"] is True), todos_info))
+    number_tasks = len(task_completed)
+    total_tasks = len(todos_info)
+
+    with open('{}.csv'.format(user_id), 'w') as csvfile:
+        csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [csvwriter.writerow([user_id, user_info["username"],
+                             task["completed"], task["title"]])
+         for task in todos_info]
